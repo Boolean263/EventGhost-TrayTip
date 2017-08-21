8a8df3a4589f0e20924e0574d0576c0c3da1d5cf
David Perry <d.perry@utoronto.ca>
2017-08-21 15:48:03 -0400

    Move WindowsBalloonTip into main file
    Allows us to act on click. Also to not freeze when a notification
    is up.

--- TrayTip/__init__.py
+++ TrayTip/__init__.py
@@ -12,14 +12,75 @@ eg.RegisterPlugin(
     guid = "{707e86ff-660f-49cd-a00a-9963a7351df0}",
 )
 
-# The WindowsBalloonTip class comes from this stackoverflow post:
-# https://stackoverflow.com/a/17262942/6692652
-from .WindowsBalloonTip import WindowsBalloonTip
-
-from threading import Thread
 import wx
-import sys
-import os.path
+
+
+import win32api
+import win32gui
+import win32con
+import winerror
+import sys, os
+
+# The WindowsBalloonTip class is adapted from this stackoverflow post:
+# https://stackoverflow.com/a/17262942/6692652
+class WindowsBalloonTip:
+    def __init__(self, title, msg, iconPathName=None):
+        message_map = {
+                win32con.WM_DESTROY: self.OnDestroy,
+                win32con.WM_USER+20: self.OnNotify,
+        }
+
+        # Register the window class.
+        wc = win32gui.WNDCLASS()
+        self.hinst = wc.hInstance = win32api.GetModuleHandle(None)
+        wc.lpszClassName = 'PythonTaskbar'
+        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        self.classAtom = None
+        try:
+            self.classAtom = win32gui.RegisterClass(wc)
+        except win32gui.error, err_info:
+            if err_info.winerror != winerror.ERROR_CLASS_ALREADY_EXISTS:
+                raise
+
+        # Create the window.
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = win32gui.CreateWindow(self.classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, self.hinst, None)
+        win32gui.UpdateWindow(self.hwnd)
+
+        # Icons managment
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+            if iconPathName:
+                hicon = win32gui.LoadImage(self.hinst, 0, win32con.IMAGE_ICON, 0, 0, icon_flags)
+            else:
+                hicon = win32gui.CreateIconFromResource(LoadResource(None, win32con.RT_ICON, 1), True)
+        except:
+            hicon = win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
+        flags = win32gui.NIF_ICON | win32gui.NIF_MESSAGE | win32gui.NIF_TIP
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, 'Tooltip')
+
+        # Notify
+        dwInfoFlags = 0x24 # NIIF_USER|NIIF_LARGE_ICON - not defined in win32con?
+        win32gui.Shell_NotifyIcon(win32gui.NIM_ADD, nid)
+        win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY, (self.hwnd, 0, win32gui.NIF_INFO, win32con.WM_USER+20, hicon, 'Balloon Tooltip', msg, 200, title, dwInfoFlags))
+
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (self.hwnd, 0)
+        #eg.PrintNotice("Destroy: msg={:08X} wparam={:08X} lparam={:08X}".format(msg, wparam, lparam))
+        win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE, nid)
+
+    def OnNotify(self, hwnd, msg, wparam, lparam):
+        #eg.PrintNotice("Notify: msg={:08X} wparam={:08X} lparam={:08X}".format(msg, wparam, lparam))
+        # Magic numbers until I learn their proper constant names:
+        # 0x0402: seems to mean the appearance of the notification
+        # 0x0404: the notification vanishes (on its own?)
+        # 0x0405: disappeared by click
+        if lparam == 0x0405:
+            eg.TriggerEvent("Clicked", payload=None, prefix="TrayTip")
+        if lparam == 0x0404 or lparam == 0x0405:
+            win32gui.DestroyWindow(self.hwnd)
+            self.classAtom = win32gui.UnregisterClass(self.classAtom, self.hinst)
+
 
 class TrayTip(eg.PluginBase):
 
@@ -34,10 +95,7 @@ class showTip(eg.ActionBase):
         title = eg.ParseString(title or "EventGhost")
         msg = eg.ParseString(msg or "")
 
-        # EventGhost freezes if we call WindowsBallonTip directly.
-        t = Thread(target=WindowsBalloonTip, args=(title, msg, icon))
-        t.daemon = True
-        t.start()
+        WindowsBalloonTip(title, msg, icon)
 
     def GetLabel(self, title, msg):
         return "{}: {}".format(title, msg)
