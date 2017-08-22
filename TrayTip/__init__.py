fc77bdb61786d104e62e6ad103955cffd679f89f
Kevin Schlosser <drschlosser@hotmail.com>
2017-08-21 22:30:11 -0600

    Formats code and changes the  action config panel

--- TrayTip/__init__.py
+++ TrayTip/__init__.py
@@ -1,23 +1,42 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of EventGhost.
+# Copyright © 2005-2016 EventGhost Project <http://www.eventghost.org/>
+#
+# EventGhost is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# EventGhost is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with EventGhost. If not, see <http://www.gnu.org/licenses/>.
 
 import eg
 
+
 eg.RegisterPlugin(
-    name = "TrayTip",
-    author = "David Perry <d.perry@utoronto.ca>",
-    version = "0.0.1",
-    kind = "other",
-    description = "Show notices in the system tray.",
-    url = "https://github.com/Boolean263/EventGhost-TrayTip",
-    guid = "{707e86ff-660f-49cd-a00a-9963a7351df0}",
+    name="TrayTip",
+    author="David Perry <d.perry@utoronto.ca>",
+    version="0.0.1",
+    kind="other",
+    description="Show notices in the system tray.",
+    url="https://github.com/Boolean263/EventGhost-TrayTip",
+    guid="{707e86ff-660f-49cd-a00a-9963a7351df0}",
 )
 
-import wx
-import win32api
-import win32gui
-import win32con
-import winerror
-import sys, os
+import wx # NOQA
+import win32api # NOQA
+import win32gui # NOQA
+import win32con # NOQA
+import winerror # NOQA
+import sys # NOQA
+import os # NOQA
+
 
 class TrayTip(eg.PluginBase):
     payloads = {}
@@ -32,7 +51,7 @@ class TrayTip(eg.PluginBase):
         wc.lpszClassName = 'EventGhostTrayTip'
         wc.lpfnWndProc = {
             win32con.WM_DESTROY: self.OnDestroy,
-            win32con.WM_USER+20: self.OnNotify,
+            win32con.WM_USER + 20: self.OnNotify,
         }
         self.classAtom = win32gui.RegisterClass(wc)
 
@@ -48,19 +67,23 @@ class TrayTip(eg.PluginBase):
         del self.payloads[hwnd]
 
     def OnNotify(self, hwnd, msg, wparam, lparam):
-        #eg.PrintNotice("Notify: msg={:08X} wparam={:08X} lparam={:08X}".format(msg, wparam, lparam))
+        # eg.PrintNotice("Notify: msg={:08X} wparam={:08X} lparam={:08X}"
+        # .format(msg, wparam, lparam))
         # Magic numbers until I learn their proper constant names:
         # 0x0402: seems to mean the appearance of the notification
         # 0x0404: the notification vanishes (on its own?)
         # 0x0405: disappeared by click
-        if lparam == 0x0405:
-            try:
-                payload = self.payloads[hwnd]
-            except KeyError:
-                payload = None
-            eg.TriggerEvent("Clicked", payload=payload, prefix='TrayTip')
-        if lparam == 0x0404 or lparam == 0x0405:
+
+        if lparam in (0x0404, 0x0405):
+            if lparam == 0x0405:
+                try:
+                    payload = self.payloads[hwnd]
+                except KeyError:
+                    payload = None
+                eg.TriggerEvent("Clicked", payload=payload, prefix='TrayTip')
+
             win32gui.DestroyWindow(hwnd)
+
 
 class showTip(eg.ActionBase):
     name = "Show system tray message"
@@ -75,27 +98,62 @@ class showTip(eg.ActionBase):
         # https://stackoverflow.com/a/17262942/6692652
         # Create the window.
         style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        hwnd = win32gui.CreateWindow(self.plugin.classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, self.plugin.hinst, None)
+        hwnd = win32gui.CreateWindow(
+            self.plugin.classAtom,
+            "TaskBar",
+            style,
+            0,
+            0,
+            win32con.CW_USEDEFAULT,
+            win32con.CW_USEDEFAULT,
+            0,
+            0,
+            self.plugin.hinst,
+            None
+        )
         win32gui.UpdateWindow(hwnd)
         self.plugin.setPayload(hwnd, payload)
 
-        # Icons managment
+        # Icons management
         iconPathName = None # for now
         try:
             if iconPathName:
                 icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-                hicon = win32gui.LoadImage(self.plugin.hinst, 0, win32con.IMAGE_ICON, 0, 0, icon_flags)
+                hicon = win32gui.LoadImage(
+                    self.plugin.hinst,
+                    0,
+                    win32con.IMAGE_ICON,
+                    0,
+                    0,
+                    icon_flags
+                )
             else:
-                hicon = win32gui.CreateIconFromResource(win32api.LoadResource(None, win32con.RT_ICON, 1), True)
+                hicon = win32gui.CreateIconFromResource(
+                    win32api.LoadResource(None, win32con.RT_ICON, 1), True)
         except:
             hicon = win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
         flags = win32gui.NIF_ICON | win32gui.NIF_MESSAGE | win32gui.NIF_TIP
-        nid = (hwnd, 0, flags, win32con.WM_USER+20, hicon, 'Tooltip')
+        nid = (hwnd, 0, flags, win32con.WM_USER + 20, hicon, 'Tooltip')
 
         # Notify
-        dwInfoFlags = 0x24 # NIIF_USER|NIIF_LARGE_ICON - not defined in win32con?
+        # NIIF_USER|NIIF_LARGE_ICON - not defined in win32con?
+        dwInfoFlags = 0x24
         win32gui.Shell_NotifyIcon(win32gui.NIM_ADD, nid)
-        win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY, (hwnd, 0, win32gui.NIF_INFO, win32con.WM_USER+20, hicon, 'Balloon Tooltip', msg, 200, title, dwInfoFlags))
+        win32gui.Shell_NotifyIcon(
+            win32gui.NIM_MODIFY,
+            (
+                hwnd,
+                0,
+                win32gui.NIF_INFO,
+                win32con.WM_USER + 20,
+                hicon,
+                'Balloon Tooltip',
+                msg,
+                200,
+                title,
+                dwInfoFlags
+            )
+        )
 
         # Window destruction is taken care of in the parent class
 
@@ -104,25 +162,55 @@ class showTip(eg.ActionBase):
 
     def Configure(self, title="", msg="", payload=""):
         panel = eg.ConfigPanel(self)
-        titleCtrl = panel.TextCtrl(title)
-        msgCtrl = panel.TextCtrl(msg)
-        payloadCtrl = panel.TextCtrl(payload)
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(panel.StaticText("Title"))
-        sizer.Add(titleCtrl, 0, wx.EXPAND|wx.TOP)
-        sizer.Add(panel.StaticText("Message"))
-        sizer.Add(msgCtrl, 0, wx.EXPAND|wx.TOP)
-        sizer.Add(panel.StaticText("Event payload if clicked (optional)"))
-        sizer.Add(payloadCtrl, 0, wx.EXPAND|wx.TOP)
+        title_st = panel.StaticText("Title:")
+        title_ctrl = panel.TextCtrl(title)
+        msg_st = panel.StaticText("Message:")
+        msg_ctrl = panel.TextCtrl(msg)
+        payload_st = panel.StaticText("Event payload if clicked (optional):")
+        payload_ctrl = panel.TextCtrl(payload)
 
-        panel.sizer.Add(sizer, 0, wx.EXPAND|wx.ALL, 10)
+        eg.EqualizeWidths((title_st, msg_st, payload_st))
+        eg.EqualizeWidths((title_ctrl, msg_ctrl, payload_ctrl))
+
+        title_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        title_sizer.Add(title_st, 0, wx.EXPAND | wx.ALL, 5)
+        title_sizer.Add(title_ctrl, 0, wx.EXPAND | wx.ALL, 5)
+
+        msg_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        msg_sizer.Add(msg_st, 0, wx.EXPAND | wx.ALL, 5)
+        msg_sizer.Add(msg_ctrl, 0, wx.EXPAND | wx.ALL, 5)
+
+        payload_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        payload_sizer.Add(payload_st, 0, wx.EXPAND | wx.ALL, 5)
+        payload_sizer.Add(payload_ctrl, 0, wx.EXPAND | wx.ALL, 5)
+
+        panel.sizer.Add(title_sizer, 0, wx.EXPAND)
+        panel.sizer.Add(msg_sizer, 0, wx.EXPAND)
+        panel.sizer.Add(payload_sizer, 0, wx.EXPAND)
+
+        # Optionally you can do this as well for adding the controls.
+        # I like it when it is keyed out. less "voodooish" code that way
+        # but it all depends on your liking.
+        #
+        # def add(*args):
+        #     for st, ctrl in args:
+        #         sizer = wx.BoxSizer(wx.HORIZONTAL)
+        #         sizer.Add(st, 0, wx.EXPAND | wx.ALL, 5)
+        #         sizer.Add(ctrl, 0, wx.EXPAND | wx.ALL, 5)
+        #         panel.sizer.Add(sizer, 0, wx.EXPAND)
+        #
+        # add(
+        #     (title_st, title_ctrl),
+        #     (msg_st, msg_ctrl),
+        #     (payload_st, payload_ctrl)
+        # )
 
         while panel.Affirmed():
             panel.SetResult(
-                titleCtrl.GetValue(),
-                msgCtrl.GetValue(),
-                payloadCtrl.GetValue(),
+                title_ctrl.GetValue(),
+                msg_ctrl.GetValue(),
+                payload_ctrl.GetValue(),
             )
 
 
